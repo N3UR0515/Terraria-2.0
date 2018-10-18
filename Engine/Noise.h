832a83e80ce7a96d06ade678228b2f12120eb0b7
Gurau Nikolas <lilitroman@yahoo.com>
2018-10-18 22:57:56 +0300

    Added Perlin Noise with cubic interpolation

--- Engine/Noise.h
+++ Engine/Noise.h
@@ -18,6 +18,19 @@ public:
 		return total;
 	}
 
+	static float PerlinNoise_1DWithCubic(float x, float freq, float ampl) // 1 octave!
+	{
+		float total = 0;
+
+		int frequency = int(freq);
+		int amplitude = int(ampl);
+
+		total += InterpolatedNoiseWithCubic(x * float(frequency)) * float(amplitude);
+
+		return total;
+	}
+
+	//This is nothing important
 	static float map(float n, float start1, float start2, float stop1, float stop2)
 	{
 		return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
@@ -44,6 +57,17 @@ private:
 		return  a * (1 - f) + b * f;
 	}
 
+	static float Cubic_Interpolation(float v0, float v1, float v2, float v3, float x)
+	{
+		float P = (v3 - v2) - (v0 - v1);
+		float Q = (v0 - v1) - P;
+		float R = v2 - v0;
+		float S = v1;
+
+		return P * pow(x, 3) + Q * pow(x, 2) + R * x + S;
+	}
+
+	//InterpolatedNoiseWithCosineInterpolation
 	static float InterpolatedNoise(float x)
 	{
 		int integer_X = int(x);
@@ -53,5 +77,20 @@ private:
 		float v2 = SmoothNoise(float(integer_X + 1));
 
 		return Cosine_Interpolate(v1, v2, fractional_X);
+	}
+public:
+	//InterpolatedNoiseWithCubicInterpolation
+	static float InterpolatedNoiseWithCubic(float x)
+	{
+		int integer_X = int(x);
+		float fractional_X = x - integer_X;
+
+		float v0 = SmoothNoise(integer_X - 1);
+		float v1 = SmoothNoise(integer_X);
+		float v2 = SmoothNoise(integer_X + 1);
+		float v3 = SmoothNoise(integer_X + 1);
+
+		return Cubic_Interpolation(v0, v1, v2, v3, fractional_X);
+
 	}
 };
